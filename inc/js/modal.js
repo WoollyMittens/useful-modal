8963fc75570454013842a14200844ee82daac1ec
Maurice van Creij <maurice.vancreij@webqem.com>
2014-07-08 14:12:50 +1000

    Reduced reliance on typography.

--- inc/js/modal.js
+++
@@ -1,7 +0,0 @@
-/*
-	Combined JavaScripts
-
-	@codekit-append "../src/useful.modal.js"
-	@codekit-append "../../useful-transitions/src/useful.transitions.js"
-	@codekit-append "../../useful-polyfills/src/useful.polyfills.js"
-*/
